0aecb0cc08a95a9e408d56ed7a4cdf37417b18ce
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-28 13:28:30 +0300

    Reservation Slice is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./app/store";
 import ReservationCard from "./components/ReservationCard";
+import { addReservation } from "./features/reservationSlice";
 
 function App() {
+  const [reservationNameInput, setReservationNameInput] = useState("");
   const reservations = useSelector(
     (state: RootState) => state.reservations.value
   );
+
+  const dispatch = useDispatch();
+  const handleAddReservations = () => {
+    if (!reservationNameInput) return;
+    dispatch(addReservation(reservationNameInput));
+    setReservationNameInput("");
+  };
   return (
     <div className="App">
       <div className="container">
@@ -21,8 +30,11 @@ function App() {
             </div>
           </div>
           <div className="reservation-input-container">
-            <input />
-            <button>Add</button>
+            <input
+              value={reservationNameInput}
+              onChange={(e) => setReservationNameInput(e.target.value)}
+            />
+            <button onClick={handleAddReservations}>Add</button>
           </div>
         </div>
         <div className="customer-food-container">
